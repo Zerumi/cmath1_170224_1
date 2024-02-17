523941e7d9965fcde4c73104a6f8b8e709ffa8bd
Zerumi <46845708+Zerumi@users.noreply.github.com>
2024-02-17 16:15:13 +0300

    chore: code formatting

--- src/main/kotlin/math/ExtendedMatrix.kt
+++ src/main/kotlin/math/ExtendedMatrix.kt
@@ -3,9 +3,10 @@ package org.example.math
 import java.math.BigDecimal
 import java.math.RoundingMode
 
-class ExtendedMatrix
-    (matrix: Matrix,
-     solutionVector : Array<BigDecimal>) : Matrix(matrix) {
+class ExtendedMatrix(
+    matrix: Matrix,
+    solutionVector: Array<BigDecimal>
+) : Matrix(matrix) {
     private fun extendMatrix(array: Array<BigDecimal>) {
         this.addMatrixCol(array)
     }
@@ -21,15 +22,17 @@ class ExtendedMatrix
         vector: Array<BigDecimal>,
         operation: (BigDecimal, BigDecimal) -> BigDecimal
     ) {
-        setMatrixElement(row, this.getDimension(),
-            operation(this.getMatrixElement(row, this.getDimension()), vector[this.getDimension()]))
+        setMatrixElement(
+            row, this.getDimension(),
+            operation(this.getMatrixElement(row, this.getDimension()), vector[this.getDimension()])
+        )
         super.applyVectorToRow(row, vector, operation)
     }
 
     override fun printMatrix() {
         for (i in 0..<this.getDimension()) {
             for (j in 0..this.getDimension()) {
-                print("${this.getMatrixElement(i,j).setScale(2, RoundingMode.FLOOR).toPlainString()} ")
+                print("${this.getMatrixElement(i, j).setScale(2, RoundingMode.FLOOR).toPlainString()} ")
             }
             println()
         }
--- src/main/kotlin/math/Matrix.kt
+++ src/main/kotlin/math/Matrix.kt
@@ -12,12 +12,12 @@ import java.math.RoundingMode
  * @since 1.0
  * @author Zerumi
  */
-open class Matrix(private val dim : Int, private val scale : Int = 32) {
+open class Matrix(private val dim: Int, private val scale: Int = 32) {
 
     protected constructor(matrix: Matrix) : this(matrix.dim, matrix.scale) {
         for (i in 0..<matrix.dim) {
             for (j in 0..<matrix.dim) {
-                matrixArray[i][j] = matrix.getMatrixElement(i,j)
+                matrixArray[i][j] = matrix.getMatrixElement(i, j)
             }
         }
     }
@@ -29,61 +29,75 @@ open class Matrix(private val dim : Int, private val scale : Int = 32) {
             }
         }
 
-    fun getDimension() : Int {
+    fun getDimension(): Int {
         return dim
     }
 
-    fun getValueScale() : Int {
+    fun getValueScale(): Int {
         return scale
     }
 
-    fun getMatrixElement(row: Int,
-                         col: Int) : BigDecimal {
+    fun getMatrixElement(
+        row: Int,
+        col: Int
+    ): BigDecimal {
         return matrixArray[row][col]
     }
 
-    fun setMatrixElement(row : Int,
-                         col : Int,
-                         element : String) {
+    fun setMatrixElement(
+        row: Int,
+        col: Int,
+        element: String
+    ) {
         matrixArray[row][col] = BigDecimal(element).setScale(scale)
     }
 
-    protected fun setMatrixElement(row : Int,
-                                  col : Int,
-                                  element : BigDecimal) {
+    protected fun setMatrixElement(
+        row: Int,
+        col: Int,
+        element: BigDecimal
+    ) {
         matrixArray[row][col] = element.setScale(scale)
     }
 
-    fun getMatrixRow(row: Int) : Array<BigDecimal> {
+    fun getMatrixRow(row: Int): Array<BigDecimal> {
         return matrixArray[row]
     }
 
-    fun getMatrixCol(col: Int) : Array<BigDecimal> {
+    fun getMatrixCol(col: Int): Array<BigDecimal> {
         return Array(dim) {
             matrixArray[it][col]
         }
     }
 
-    fun setMatrixRow(row : Int,
-                     vector : Array<BigDecimal>) {
+    fun setMatrixRow(
+        row: Int,
+        vector: Array<BigDecimal>
+    ) {
         matrixArray[row] = vector
     }
 
-    fun setMatrixCol(col : Int,
-                     vector: Array<BigDecimal>) {
+    fun setMatrixCol(
+        col: Int,
+        vector: Array<BigDecimal>
+    ) {
         for (i in 0..<dim) {
             matrixArray[i][col] = vector[i]
         }
     }
 
-    protected fun addMatrixCol(vector: Array<BigDecimal>) {
+    protected fun addMatrixCol(
+        vector: Array<BigDecimal>
+    ) {
         for (i in 0..<dim) {
             matrixArray[i] = matrixArray[i].plus(vector[i])
         }
     }
 
-    fun swapRows(row1 : Int,
-                 row2 : Int) {
+    fun swapRows(
+        row1: Int,
+        row2: Int
+    ) {
         for (i in 0..<dim) {
             val temp = matrixArray[row1][i]
             matrixArray[row1][i] = matrixArray[row2][i]
@@ -91,8 +105,10 @@ open class Matrix(private val dim : Int, private val scale : Int = 32) {
         }
     }
 
-    fun swapCols(col1 : Int,
-                 col2 : Int) {
+    fun swapCols(
+        col1: Int,
+        col2: Int
+    ) {
         for (i in 0..<dim) {
             val temp = matrixArray[i][col1]
             matrixArray[i][col1] = matrixArray[i][col2]
@@ -100,23 +116,29 @@ open class Matrix(private val dim : Int, private val scale : Int = 32) {
         }
     }
 
-    fun mapRow(row : Int,
-               mapFunction : (BigDecimal) -> BigDecimal) {
+    fun mapRow(
+        row: Int,
+        mapFunction: (BigDecimal) -> BigDecimal
+    ) {
         for (i in 0..<dim) {
             matrixArray[row][i] = mapFunction(matrixArray[row][i])
         }
     }
 
-    fun mapCol(col : Int,
-               mapFunction: (BigDecimal) -> BigDecimal) {
+    fun mapCol(
+        col: Int,
+        mapFunction: (BigDecimal) -> BigDecimal
+    ) {
         for (i in 0..<dim) {
             matrixArray[i][col] = mapFunction(matrixArray[i][col])
         }
     }
 
-    open fun applyVectorToRow(row : Int,
-                         vector : Array<BigDecimal>,
-                         operation : (BigDecimal, BigDecimal) -> BigDecimal) {
+    open fun applyVectorToRow(
+        row: Int,
+        vector: Array<BigDecimal>,
+        operation: (BigDecimal, BigDecimal) -> BigDecimal
+    ) {
 
         for (i in 0..<dim) {
             matrixArray[row][i] = operation(matrixArray[row][i], vector[i])
