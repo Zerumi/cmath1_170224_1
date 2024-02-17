2318930902c42184e25ed237bd6a9c48a774e206
Zerumi <46845708+Zerumi@users.noreply.github.com>
2024-02-17 03:02:58 +0300

    feat: diagonalize matrix util add

---
+++ src/main/kotlin/math/MatrixUtils.kt
@@ -0,0 +1,29 @@
+package org.example.math
+
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+class MatrixUtils {
+    companion object {
+        fun diagMatrix(matrix: Matrix) {
+            assert(matrix.getMatrixElement(0,0) != BigDecimal("0"))
+
+            for (i in 0..<matrix.getDimension()) {
+                for (j in (i + 1)..<matrix.getDimension()) {
+                    val firstVector = matrix.getMatrixRow(i)
+
+                    val removingCoefficient = matrix.getMatrixElement(j, i)
+                        .divide(matrix.getMatrixElement(i, i), RoundingMode.FLOOR).negate()
+
+                    val modifiedVector = firstVector.map {
+                        it.multiply(removingCoefficient)
+                    }.toTypedArray()
+
+                    matrix.applyVectorToRow(j, modifiedVector, BigDecimal::plus)
+                }
+            }
+        }
+
+
+    }
+}
